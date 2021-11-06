a3d08d3b99a4d0f04605e153c41b67f96f2ff907
Pedro <pedro-vicenzo@hotmail.com>
2021-11-06 03:12:24 -0300

    Alteracoes no formato do arquivo carrinho

--- carrinho/script/script.js
+++ carrinho/script/script.js
@@ -6,49 +6,53 @@ window.onload = function(){
 function MontarCarrinho(){
     var caixa = JSON.parse(window.localStorage.getItem("carrinho"));
 
-    document.getElementById("tabelaCarrinho").innerHTML = "";
+    document.getElementById("externa").innerHTML = "";
 
-    var conteudo = ""
-    conteudo += '<table>';
-    conteudo += '<tr>';
-    conteudo += '<td>' + 'Imagem do produto' + '</td>';
-    conteudo += '<td>' + 'Você está comprando' + '</td>';
-    conteudo += '<td>' + 'Unidades' + '</td>';
-    conteudo += '<td>' + 'Preço (R$)' + '</td>';
-    conteudo += '<td>' + 'Total (R$)' + '</td>';
-    conteudo += '</tr>';
-    document.getElementById("tabelaCarrinho").innerHTML += conteudo;
     if(caixa != null){
         for(var i = 0; i < caixa.length; i++){
-            conteudo = ""
-            conteudo += '<tr>';
-            conteudo += '<td>';
-            conteudo += '<center> <img src="CSS/' + caixa[i][2] + '"/> </center>';
-            conteudo += '</td>';
-            conteudo += '<td>';
-            conteudo += '<center>'+caixa[i][1]+'</center>';
-            conteudo += '</td>';
-            conteudo += '<td>';
-            conteudo += '<center>'+caixa[i][4]+'</center>';
-            conteudo += '</td>';
-            conteudo += '<td>';
-            conteudo += '<center>'+caixa[i][5]+'</center>';
-            conteudo += '</td>';
-            conteudo += '<td>';
-            conteudo += '<center>'+caixa[i][5]*caixa[i][4]+'</center>';
-            conteudo += '</td>';
-            conteudo += '</tr>';
-            document.getElementById("tabelaCarrinho").innerHTML += conteudo;
+        var conteudo = ""
+        conteudo += '<div class="cards">';
+        conteudo += '<div class="imagens">';
+        conteudo += '<img src="CSS/' + caixa[i][2] + '"/>';
+        conteudo += '</div>';
+        conteudo += '<div class="detalhes">';
+        conteudo += '<b><p>Produto: '+caixa[i][1]+'</b></p>';
+        conteudo += '</div>';
+        conteudo += '<div class="unidade">';
+        conteudo += '<b><p>Unidades: '+caixa[i][4]+'</b></p>';
+        conteudo += '</div>';
+        conteudo += '<div class="preco">';
+        conteudo += '<b><p>Preço: R$'+caixa[i][5]+'</b></p>';
+        conteudo += '</div>';
+        conteudo += '<div class="total">'
+        conteudo += '<center><b><p>TOTAL</p></b></center>';
+        conteudo += '</div>'
+        conteudo += '<div class="valortotal">';
+        conteudo += '<center>'+caixa[i][5]*caixa[i][4]+'</center>';
+        document.getElementById("externa").innerHTML += conteudo;
+        }
     }
-}
+
 else{
     conteudo  = ""
-    conteudo += '<h2> Carrinho vazio!! <h2>';
+    conteudo += '<div class="cardsvazio">';
+    conteudo += '<div class="mensagem">';
+    conteudo += '<b><p> Seu carrinho está vazio. Clique no botão adicionar itens abaixo para consultar nossos produtos!</p></b>';
+    conteudo += '</div>';
+    conteudo += '<div class="sad">';
     conteudo += '<img src="CSS/sad.jfif"/> <br>';
-    conteudo += '<button type="button" id="volta" onclick="retorno()"> Adicionar itens </button>'
-    document.getElementById("divCarrinhoVazio").innerHTML += conteudo;
- }
+    conteudo += '</div>';
+    conteudo += '<button type="button" id="maisprodt" onclick="retorno()"> Adicionar Produtos </button>'
+    conteudo += '</div>';
+    conteudo += '<div class="saida">'
+    conteudo += '<button type="button" id="voltarmenu" onclick="volta()"> Voltar </button>'
+    conteudo += '<img src="CSS/dracoins.png" id="logofinal" />'
+    conteudo += '<button type="button" id="finalizacompra" onclick="compra()"> Finalizar Compra</button>'
+    conteudo += '</div>'
+    document.getElementById("externa").innerHTML += conteudo;
 }
+}
+
 
 
 function retorno(){
